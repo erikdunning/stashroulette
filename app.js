86bfb3148a2ab5b1f1ba88b640bd4ecca3d025ba
Erik Dunning <erikdunning@gmail.com>
2016-06-08 03:18:52 +0000

    Some more progress.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-let express = require('express');
-let path = require('path');
-let config = require('./config.js');
-let app = express();
+let nodemailer  = require('nodemailer');
+let express     = require('express');
+let path        = require('path');
+let config      = require('./config.js');
+let app         = express();
 
 app.use(express.static('public'));
 app.set('views', __dirname + '/views')
@@ -11,6 +12,33 @@ app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
     res.render('index', config);
+});
+
+app.post('/signup', (req, res) => {
+/*
+    exports.send_error_email = function( errors ){
+
+    let transporter = nodemailer.createTransport('smtp://mail.badgermeter.com');
+     
+    let mailOptions = {
+        from: '"Badger Meter Workday Service" <noreply@badgermeter.com>', // sender address 
+        to: cfg.error_emails.join(', '), // list of receivers 
+        subject: 'Encountered Workday Submission Errors', // Subject line 
+        text: errors //, // plaintext body 
+        /* html: '<b>Hello world 🐴</b>' // html body 
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+
+*/
+
+});
+
 });
 
 let port = process.argv.length > 2 ? parseInt( process.argv[2] ) : 80;
